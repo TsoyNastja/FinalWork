e5e903964abb47c44a74038551bcd9c766bbcd03
Nastja <coj-nastja@mail.ru>
2022-10-04 22:37:35 +0300

    Написала ввод исходных данных

--- Program.cs
+++ Program.cs
@@ -1,2 +1,31 @@
 ﻿// 
-Console.WriteLine("Hello, World!");
+
+
+
+void ShowArray(string[] array, string title){
+    Console.WriteLine(title);
+    for(int i = 0; i<array.Length; i++){
+        Console.Write($"{array[i]} ");
+    }
+}
+
+string[] inputArray(){
+
+    Console.Write("Введите количество строк: ");
+    int size = Convert.ToInt32(Console.ReadLine());
+
+
+
+    string[] result = new string[size];
+
+    for(int i = 0; i < size; i++){
+        Console.Write($"Введите строку {i}: ");
+        result[i] = Console.ReadLine();
+    }
+
+    return result;
+}
+
+
+string[] srcArray = inputArray();
+ShowArray(srcArray, "Исходный массив");
